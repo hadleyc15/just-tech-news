ea373347e3ff07d36ab954d07ed02f093cc184a8
Chris Hadley <hadleyc15@yahoo.com>
2020-08-02 13:56:46 -0600

    updates to include dashboard, edit, and delete posts

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
@@ -18,9 +21,8 @@ const sess = {
   })
 };
 
-
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,10 +31,6 @@ app.use(session(sess));
 
 // turn on routes
 app.use(routes);
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
